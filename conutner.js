5d814cee0bb72c89f842bfefc8f8bbb56398e3d8
Classic8763 <163006402+Classic8763@users.noreply.github.com>
2024-03-31 17:07:20 +0800

    upload

---
+++ conutner.js
@@ -0,0 +1,219 @@
+I2.addEventListener("click", function() {
+    display.value = "  /";
+    display.style.backgroundImage = "url('p/VR.png')";
+    display.style.backgroundSize = "50px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 5px  center ";
+    window.scrollTo(0, 0);
+});
+V.addEventListener("click", function() {
+    display.value = " *";
+    display.style.backgroundImage = "url('p/IR.png')";
+    display.style.backgroundSize = "45px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 9px  center ";
+    window.scrollTo(0, 0);
+});
+V2.addEventListener("click", function() {
+    display.value = "  /";
+    display.style.backgroundImage = "url('p/WQ.png')";
+    display.style.backgroundSize = "55px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 0px  center ";
+    window.scrollTo(0, 0);
+});
+R.addEventListener("click", function() {
+    display.value = "  /";
+    display.style.backgroundImage = "url('p/VI.png')";
+    display.style.backgroundSize = "45px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+P.addEventListener("click", function() {
+    display.value = " ^2/";
+    display.style.backgroundImage = "url('p/P.png')";
+    display.style.backgroundSize = "80px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 0px  center ";
+    window.scrollTo(0, 0);
+});
+P2.addEventListener("click", function() {
+    display.value = " ^2*";
+    display.style.backgroundImage = "url('p/P2.png')";
+    display.style.backgroundSize = "78px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 4px  center ";
+    window.scrollTo(0, 0);
+});
+Y.addEventListener("click", function() {
+    display.value = "  *    /  +   +";
+    display.style.backgroundImage = "url('p/ys4.png')";
+    display.style.backgroundSize = "185px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+Y2.addEventListener("click", function() {
+    display.value = "  *   +  *   +   *   / ";
+    display.style.backgroundImage = "url('p/ys5.png')";
+    display.style.backgroundSize = "255px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+
+C.addEventListener("click", function() {
+    display.value = "  /";
+    display.style.backgroundImage = "url('p/QV.png')";
+    display.style.backgroundSize = "56px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+Q.addEventListener("click", function() {
+    display.value = "  *";
+    display.style.backgroundImage = "url('p/CV.png')";
+    display.style.backgroundSize = "60px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+Q2.addEventListener("click", function() {
+    display.value = " *";
+    display.style.backgroundImage = "url('p/It.png')";
+    display.style.backgroundSize = "50px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+t.addEventListener("click", function() {
+    display.value = "  *  /";
+    display.style.backgroundImage = "url('p/CVI.png')";
+    display.style.backgroundSize = "75px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+W.addEventListener("click", function() {
+    display.value = "1/2*  *";
+    display.style.backgroundImage = "url('p/VQ.png')";
+    display.style.backgroundSize = "55px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 60px  center ";
+    window.scrollTo(0, 0);
+});
+W2.addEventListener("click", function() {
+    display.value = "1/2*  *  ^";
+    display.style.backgroundImage = "url('p/CV.png')";
+    display.style.backgroundSize = "53px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 60px  center ";
+    window.scrollTo(0, 0);
+});
+L.addEventListener("click", function() {
+    display.value = " /";
+    display.style.backgroundImage = "url('p/L.png')";
+    display.style.backgroundSize = "50px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 0px  center ";
+    window.scrollTo(0, 0);
+});
+
+acc.addEventListener("click", function() {
+    display.value = " *(1-e^- )";
+    display.style.backgroundImage ="url('p/lr.png')";
+    display.style.backgroundSize = "120px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left -1px  center ";
+    window.scrollTo(0, 0);
+});
+accr.addEventListener("click", function() {
+    display.value = " *e^-";
+    display.style.backgroundImage = "url('p/lEt.png')";
+    display.style.backgroundSize = "80px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 3px  center ";
+    window.scrollTo(0, 0);
+});
+acci.addEventListener("click", function() {
+    display.value = " /  *e^-";
+    display.style.backgroundImage = "url('p/ert.png')";
+    display.style.backgroundSize = "108px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 1px  center ";
+    window.scrollTo(0, 0);
+});
+acc2.addEventListener("click", function() {
+    display.value = " *e^-";
+    display.style.backgroundImage = "url('p/lEt.png')";
+    display.style.backgroundSize = "80px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+accr2.addEventListener("click", function() {
+    display.value = "-  *(1-e^- )";
+    display.style.backgroundImage ="url('p/lr.png')";
+    display.style.backgroundSize = "120px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 18px  center ";
+    window.scrollTo(0, 0);
+});
+acci2.addEventListener("click", function() {
+    display.value = "-  /  *(1-e^- )";
+    display.style.backgroundImage = "url('p/rr.png')";
+    display.style.backgroundSize = "145px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 19px  center ";
+    window.scrollTo(0, 0);
+});
+
+acl.addEventListener("click", function() {
+    display.value = " *e^-";
+    display.style.backgroundImage = "url('p/lEt.png')";
+    display.style.backgroundSize = "80px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 2px  center ";
+    window.scrollTo(0, 0);
+});
+aclr.addEventListener("click", function() {
+    display.value = " *(1-e^- )";
+    display.style.backgroundImage ="url('p/lr.png')";
+    display.style.backgroundSize = "120px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 0px  center ";
+    window.scrollTo(0, 0);
+});
+acli.addEventListener("click", function() {
+    display.value = " /  *(1-e^- )";
+    display.style.backgroundImage = "url('p/ER.png')";
+    display.style.backgroundSize = "145px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 0px  center ";
+    window.scrollTo(0, 0);
+});
+acl2.addEventListener("click", function() {
+    display.value = "-  *e^-";
+    display.style.backgroundImage = "url('p/ET.png')";
+    display.style.backgroundSize = "85px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 18px  center ";
+    window.scrollTo(0, 0);
+});
+aclr2.addEventListener("click", function() {
+    display.value = " *e^-";
+    display.style.backgroundImage = "url('p/lEt.png')";
+    display.style.backgroundSize = "80px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 3px  center ";
+    window.scrollTo(0, 0);
+});
+acli2.addEventListener("click", function() {
+    display.value = " /  *e^-";
+    display.style.backgroundImage = "url('p/ert.png')";
+    display.style.backgroundSize = "108px";
+    display.style.backgroundRepeat = "no-repeat";
+    display.style.backgroundPosition = "left 1px  center ";
+    window.scrollTo(0, 0);
+});
